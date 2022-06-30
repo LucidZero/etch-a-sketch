86561c85fae261cc60a1756790d3f17b637961f6
Aleksandar Pavlovic <sweetsare42@gmail.com>
2022-06-30 12:51:27 +0200

    implemented that on hover blocks change color, now tryna implement that on submit button size of amount of divs change

--- javascript.js
+++ javascript.js
@@ -11,25 +11,27 @@ by certain amount.
 */
 
 let etchTilesBox = document.querySelector("#etchTilesBoxElement");
-let inputTiles = 16;
-let numberOfTiles = inputTiles * inputTiles;
+let inputTiles;
 let widthHeightTile;
 let tileHover = document.querySelector(".tile");
 
 
 
 
-
+function userInput(inputTiles){
+return inputTiles
+}
+console.log(inputTiles)
+checkIfUserInputValid()
+function checkIfUserInputValid(){
 if (inputTiles<=100){
-    generateTiles();
+    generateTiles(numberOfTiles);
     } else{
         /*in input slot make it say "only tiles between 1 and 100" */
     }
-
-/*generate a div class="tile" that will have a width and height of widthHeightTile
-that on hover change color
-*/
+};
 function generateTiles(){
+
 for (i=0; i <numberOfTiles; i++){
 const div = document.createElement("div");
 div.classList.add("tile");
@@ -43,12 +45,10 @@ document.querySelector("#etchTilesBoxElement").appendChild(div);
 etchTilesBox.addEventListener('mouseover', tileHovering);
 
 
-function tileHovering(evt) {
-    if (evt.target.nodeName === "div")
-        console.log("Responding")
-     else {
-        console.log("not responding")
-    }
+function tileHovering() {
+
+        document.querySelector(".tile:hover").style.backgroundColor = "red";
+
 }
 
 
