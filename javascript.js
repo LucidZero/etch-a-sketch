37b075dcb0c4e4be336399b5d5194d05150d8a88
Aleksandar Pavlovic <sweetsare42@gmail.com>
2022-06-30 19:53:38 +0200

    Made rainbow, and fixed my box issues. Only make CSS prettier

--- javascript.js
+++ javascript.js
@@ -11,32 +11,39 @@ by certain amount.
 */
 
 let etchTilesBox = document.querySelector("#etchTilesBoxElement");
-let inputTiles;
+let inputTiles =16;
 let widthHeightTile;
 let tileHover = document.querySelector(".tile");
 
 
+generateTiles(inputTiles)
 
 
-function userInput(inputTiles){
-return inputTiles
-}
-console.log(inputTiles)
-checkIfUserInputValid()
-function checkIfUserInputValid(){
+function checkIfUserInputValid(inputTiles){
 if (inputTiles<=100){
-    generateTiles(numberOfTiles);
+    if (inputTiles<1){
+
+        etchTilesBox.textContent = ""; 
+        inputTiles=1;
+        generateTiles(inputTiles);
+     } else if (inputTiles<=100){
+
+        etchTilesBox.textContent = ""; 
+        generateTiles(inputTiles);
     } else{
-        /*in input slot make it say "only tiles between 1 and 100" */
+        document.getElementById('chooseTiles').value = "100";
     }
-};
-function generateTiles(){
+}
+}
+function generateTiles(inputTiles){
+
+let numberOfTiles = inputTiles * inputTiles;
 
 for (i=0; i <numberOfTiles; i++){
 const div = document.createElement("div");
 div.classList.add("tile");
-div.style.width = (320 / inputTiles) - 0.8 + "px";
-div.style.height = (320 / inputTiles) - 0.8 + "px";
+div.style.width = (320 / inputTiles) - 2 + "px";
+div.style.height = (320 / inputTiles) - 2 + "px";
 document.querySelector("#etchTilesBoxElement").appendChild(div);
 }
 }
@@ -45,10 +52,45 @@ document.querySelector("#etchTilesBoxElement").appendChild(div);
 etchTilesBox.addEventListener('mouseover', tileHovering);
 
 
+
+let rainbowColors = 60;
+let hues = 30;
+let blues = 210;
+let h = 380;
+
+/*my red cm blue cy greem */
 function tileHovering() {
+    
+    
+    if ( rainbowColors > 200) {
+        rainbowColors = 60;
+        hues = 30;
+        blues = 210;
+    } 
+    let colorPickerValue = document.getElementById('colorPicker').value;
+    
+    if (colorPickerValue === "bandw"){
+        rainbowColors +=10;
+            document.querySelector(".tile:hover").style.backgroundColor = "rgb(" + rainbowColors + "," + rainbowColors + "," + rainbowColors + ")";
+        
+        
+    }  else if (colorPickerValue === "gandm"){
+        rainbowColors +=10;
+        hues +=10;
+        blues -=10;
+        document.querySelector(".tile:hover").style.backgroundColor = "rgb(" + rainbowColors + "," + blues + "," + hues + ")";
+    } else if (colorPickerValue === "rainbow"){
+        h -= 20;
+        if (h<=0){
+            h=360;
+        }
 
-        document.querySelector(".tile:hover").style.backgroundColor = "red";
+        document.querySelector(".tile:hover").style.backgroundColor = "hsl("+ h +", 100%, 50%)";
 
+   
+    } else{
+        document.querySelector(".tile:hover").style.backgroundColor = colorPickerValue;
+    }
 }
 
 
